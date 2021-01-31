41ca0f2f79126ce1b14b6087dedfb6780d8586c2
pkuzw <zhaowei.pku@gmail.com>
2021-01-31 21:38:50 +0800

    LeetCode Hot 100, Problem 1. TwoSum, Python

---
+++ TwoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = list()
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+                    return result
+        return result
